ef0115dd426cb44271cd77f4868597ad7f462c52
Hanae_Gourier <hgourie1@jaguarlandrover.com>
2021-10-15 14:59:30 +0100

    Week 4 changes

--- code/crud.py
+++ code/crud.py
@@ -1,60 +1,73 @@
 import utilities
 
-def add_item(sub_menu_item, list): 
-    utilities.clear_screen() 
-    while True:
-        new_item=input(f"Please type new {sub_menu_item.lower()} name: \n").title()
-        if utilities.check_duplicate(list, new_item):
-            print(f"Error, {sub_menu_item.lower()} already in list")
-            break
-        else:
-            list.append(new_item)
-            utilities.clear_screen()
-            print(list)
-            break
-
-def add_order(list1,list2):
+def add_item(sub_menu_item, list1, list2,list3): 
     utilities.clear_screen()
-    new_item=get_new_order(list2)
-    list1.append(new_item)
-    utilities.print_position_list(list1)
-
-def get_new_order(list):
-    customer_name= input("Please input customer name: ").title()
-    customer_address= input("Please input customer address: ").title()
-    customer_phone= input("Please input customer phone: ")
-    utilities.print_position_list(list) 
-    chosen_courier=input("\n Please input index of courier chosen for this order: ")
-    order_status="PREPARING"
-    order={
-            "customer_name": customer_name,
-            "customer_address": customer_address,
-            "customer_phone": customer_phone,
-            "courier": chosen_courier,
-            "status": order_status
-            }  
-    return order
+    if sub_menu_item == "Product":
+        while True:
+            new_item_name=input(f"Please type new {sub_menu_item.lower()} name: \n").title()
+            new_item_price=float(input(f"Please type new {sub_menu_item.lower()} price: \n"))
+            if utilities.check_duplicate(list1, new_item_name,"name"):
+                print(f"Error, {sub_menu_item.lower()} already in list")
+                break
+            else:
+                new_dict={"name": new_item_name, "price": new_item_price}
+                list1.append(new_dict)
+                utilities.clear_screen()
+                utilities.print_position_list(list1)
+                break
+    elif sub_menu_item == "Courier":
+        while True:
+            new_item_name=input(f"Please type new {sub_menu_item.lower()} name: \n").title()
+            new_item_phone=input(f"Please type new {sub_menu_item.lower()} phone: \n")
+            if utilities.check_duplicate(list1, new_item_name,"name"):
+                print(f"Error, {sub_menu_item.lower()} already in list")
+                break
+            else:
+                new_dict= {"name": new_item_name, "phone": new_item_phone}
+                list1.append(new_dict)
+                utilities.clear_screen()
+                utilities.print_position_list(list1)
+                break
+    elif sub_menu_item == "Orders":
+        while True:
+            new_customer_name=input(f"Please type new {sub_menu_item.lower()} cutomer name: \n").title()
+            new_customer_address=input(f"Please type new {sub_menu_item.lower()} customer address: \n").title()
+            new_customer_phone=input(f"Please type new {sub_menu_item.lower()} customer phone: \n")
+            utilities.print_position_list(list2) 
+            new_courier=input("\n Please input index of courier chosen for this order: ")
+            utilities.print_position_list(["Placed", "Preparing", "Being delivered", "Delivered", "Cancelled"]) 
+            new_status=input(f"\n Please type index of order status: \n").upper()
+            utilities.print_position_list(list3) 
+            new_items=input(f"Please type new {sub_menu_item.lower()} list of items: \n").title()
+            new_dict={"customer_name": new_customer_name, "customer_address": new_customer_address, "customer_phone":new_customer_phone, "courier":new_courier, "status":new_status, "items":new_items}
+            list1.append(new_dict)
+            utilities.clear_screen()
+            utilities.print_position_list(list1)
+            break
 
 def update_item(sub_menu_item, list):
     utilities.clear_screen()
     while True:
         utilities.print_position_list(list)
-        position=int(input(f"\n Please type position in the list of {sub_menu_item.lower()} to be modified: "))-1
-        if position >=len(list):
+        modified_dict_index=int(input(f"\n Please input index of the item to be modified: "))-1
+        if modified_dict_index >=len(list):
             print("\n Error, number not available in list\n")
         else:
-            modified_item=input(f"\n Please type new {sub_menu_item.lower()} name for the chosen position: \n").title()
-            print("\n")
-            if utilities.check_duplicate(list, modified_item):
-                print(f"Error, {sub_menu_item.lower()} already in list")
-                break
+            utilities.print_dict(list[modified_dict_index])
+            modified_item_index=int(input(f"\n Please input index of the item to be modified: \n"))-1
+            if modified_dict_index >=len(list):
+                print("\n Error, number not available in list\n")#need to prevent choosing index not in list
             else:
+                modified_item_value=input(f"\n Please input new value of the item to be modified: \n").title()
+                dictionary = list[modified_dict_index]
+                list_of_keys = utilities.get_list_of_dict_keys(dictionary)
+                key_being_modified=list_of_keys[modified_item_index]
+                dictionary[key_being_modified]=modified_item_value
                 utilities.clear_screen()
-                list[position]=modified_item
                 utilities.print_position_list(list)
-                break
+                break                
 
-def delete_item(sub_menu_item, list, position=None, confirmation=None):   #default arguments - if not called will be this value
+def delete_item(sub_menu_item, list, position=None, confirmation=None):
     utilities.clear_screen()
     while True:
         utilities.print_position_list(list)
@@ -68,28 +81,23 @@ def delete_item(sub_menu_item, list, position=None, confirmation=None):   #defau
             utilities.print_position_list(list)
             break
         else:
-            print(f"Error, {sub_menu_item.lower()} already in list")
+            #print(f"Error, {sub_menu_item.lower()} already in list")
             break
 
-def update_order(list, position=None, category=None, new_category_input=None):
+def update_order_status(sub_menu_item, list):
     utilities.clear_screen()
-    utilities.print_position_list(list)
-    if position is None:
-        position=int(input(f"\n Please type position in the list of order to be modified: "))-1
-        if position >=len(list):
+    while True:
+        utilities.print_position_list(list)
+        modified_dict_index=int(input(f"\n Please input index of the {sub_menu_item.lower()} for which status is to be modified: "))-1
+        if modified_dict_index >=len(list):
             print("\n Error, number not available in list\n")
         else:
-            utilities.print_dict(list[position])
-            if category is None:
-                category=(input("Please type position in the list of the item you would like to modify: \n"))
-            if category == "status":
-                if new_category_input is None:
-                    new_category_input=(input("Please type in the new order status: \n")).upper()           
-            else:
-                if new_category_input is None:
-                    new_category_input=(input("Please type in the new value: \n")).title()
-    list[position][category]=new_category_input
-    utilities.clear_screen()
-
-def update_order_status(list):
-    update_order(list, category="status")
+            status_list=["Placed", "Preparing", "Being delivered", "Delivered", "Cancelled"]
+            utilities.print_position_list(status_list) 
+            modified_item_index=int(input(f"\n Please input index of new status: \n"))-1
+            modified_item_value=status_list[modified_item_index]
+            dictionary = list[modified_dict_index]
+            dictionary["status"]=modified_item_value
+            utilities.clear_screen()
+            utilities.print_position_list(list)
+            break 
